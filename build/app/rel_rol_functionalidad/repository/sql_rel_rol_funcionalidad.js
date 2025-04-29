5d0ed84400647bce2af5b2c1de624d23274f3ebe
Sebastian Ruiz <110864059+Saruizz@users.noreply.github.com>
2025-04-29 01:17:55 -0500

    Delete build directory

--- build/app/rel_rol_functionalidad/repository/sql_rel_rol_funcionalidad.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const SQL_REL_ROL_FUNCIONALIDAD = {
-    getAll: 'SELECT * FROM rel_rol_funcionalidad',
-    create: 'INSERT INTO rel_rol_funcionalidad (cod_rol, cod_funcionalidad) VALUES (?, ?)',
-    update: 'UPDATE rel_rol_funcionalidad SET cod_rol = ?, cod_funcionalidad = ? WHERE cod_rol = ? AND cod_funcionalidad = ?',
-    delete: 'DELETE FROM rel_rol_funcionalidad WHERE cod_rol = ? AND cod_funcionalidad = ?',
-};
-exports.default = SQL_REL_ROL_FUNCIONALIDAD;
--- build/config/domain/ValidarRelTurnoUsuario.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.datosRelTurnoUsuario = void 0;
-const express_validator_1 = require("express-validator");
-exports.datosRelTurnoUsuario = [
-    (0, express_validator_1.body)("cod_turno").isInt().withMessage("El código del turno debe ser un número entero"),
-    (0, express_validator_1.body)("cod_usuario").isInt().withMessage("El código del usuario debe ser un número entero")
-];
