e7bce399c521dbecce6c353de1fb37615d0e9dca
Andres Licona Mahcado <andresmauricio745@gmail.com>
2025-05-05 19:39:03 -0500

    correccion en accesos

--- src/app/accesos/repository/sql_accesos.ts
+++ src/app/accesos/repository/sql_accesos.ts
@@ -1,7 +1,7 @@
 export const sql_accesos = {
   getAll: `SELECT * FROM accesos`,
   getById: `SELECT * FROM accesos WHERE cod_usuario = $1`,
-  create: `INSERT INTO accesos (cod_usuario, correo_acceso, clave_acceso, uuid_acceso) VALUES ($1, $2, $3, $4)`,
+  create: `INSERT INTO accesos (cod_usuario, correo_acceso, clave_acceso, uuid_acceso) VALUES ($1, $2, $3, $4) RETURNING *`,
   update: `UPDATE accesos SET correo_acceso = $2, clave_acceso = $3, uuid_acceso = $4 WHERE cod_usuario = $1`,
   delete: `DELETE FROM accesos WHERE cod_usuario = $1`,
   getByCorreo: `SELECT * FROM accesos WHERE correo_acceso = $1`,
